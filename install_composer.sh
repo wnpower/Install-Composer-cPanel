aa6db2862b642efdda6eed2b314c9b500046b06a
Ignacio Morandin <35805127+imorandinwnp@users.noreply.github.com>
2022-06-01 15:04:05 -0300

    Update install_composer.sh

--- install_composer.sh
+++ install_composer.sh
@@ -11,7 +11,6 @@ do
 	rm -rf ~/bin/composer
 	mkdir -p ~/bin/composer
 	/opt/cpanel/$PHP_VER/root/usr/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	#php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	cd ~
 	/opt/cpanel/$PHP_VER/root/usr/bin/php -d disable_functions="" composer-setup.php --install-dir=./bin/composer
 	/opt/cpanel/$PHP_VER/root/usr/bin/php -r "unlink('composer-setup.php');"
